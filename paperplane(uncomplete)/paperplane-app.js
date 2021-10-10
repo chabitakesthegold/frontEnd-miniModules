29dc62c5e67bb8fc330f358e4e14d4d3eae50d0c
chabitakesthegold <nderituchabi@gmail.com>
2021-10-10 21:56:07 +0300

    Add files via upload

---
+++ paperplane(uncomplete)/paperplane-app.js
@@ -0,0 +1,14 @@
+const flightPath={
+    curviness:1.25,
+    autoRotate:true,
+    values:[{ x:100, y:-20 },{ x:100, y:10 }]
+};
+
+const tween= new TimelineLite();
+
+tween.add(
+TweenLite.to(".paper-plane",1,{
+    bezier:flightPath,
+    ease:Power1.easeInOut
+})
+);
